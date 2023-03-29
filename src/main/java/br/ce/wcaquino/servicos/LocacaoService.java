5e2a00536b46b783930c23c94f91862f69d4df09
biangomes <beanascigom@gmail.com>
2023-03-29 19:08:18 -0300

    O método test() foi movido para uma classe LocacaoServiceTest

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,12 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
-import org.junit.Test;
-
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -29,23 +26,5 @@ public class LocacaoService {
 		//TODO adicionar m√©todo para salvar
 		
 		return locacao;
-	}
-
-	
-	@Test
-	public void teste() {
-		// Cen·rio
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Beatriz");
-		Filme filme = new Filme("John Wick III", 2, 5.00);
-		
-		// Acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		// Verificacao
-		System.out.println(locacao.getValor() == 2.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(2)));
-	}
-	
+	}	
 }
---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.servicos;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTest {
+	
+	@Test
+	public void teste() {
+		// Cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Beatriz");
+		Filme filme = new Filme("John Wick III", 2, 5.00);
+		
+		// Acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		// Verificacao
+		Assert.assertTrue(locacao.getValor() == 5.00);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
